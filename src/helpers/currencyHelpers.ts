26751d7aaa741fe151030de93a9afc9d497e7b97
Alexey Ardov <ardovalexey@gmail.com>
2022-02-20 23:49:52 +0300

    get more precise (4 digits now)

--- src/helpers/currencyHelpers.ts
+++ src/helpers/currencyHelpers.ts
@@ -1,1 +1,5 @@
-export const round = (amount: number): number => Math.round(amount * 100) / 100
+// Using 4 digits after the dot is the same precision that ZenMoney allows
+const PRECISION = 4 ** 10
+
+export const round = (amount: number): number =>
+  Math.round(amount * PRECISION) / PRECISION
